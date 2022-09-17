d10e3efc9350e637e58b3fb804de811db5f3d196
akshaymishra07 <akshayk.kvhec@gmail.com>
2022-09-17 16:47:10 +0530

    second pass

--- src/com/parkinglot/model/ParkingLot.java
+++ src/com/parkinglot/model/ParkingLot.java
@@ -2,48 +2,49 @@ package com.parkinglot.model;
 
 import com.parkinglot.exception.SlotNotAvailableException;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
 
-    private int capacity;
-    private Map<Integer, Car> slots;
+  private int capacity;
+  private Map<Integer, Car> slots;
 
-    public ParkingLot(int capacity) {
-        this.capacity = capacity;
+  public ParkingLot(int capacity) {
+    this.capacity = capacity;
+    slots = new HashMap<>();
+  }
+
+  public int getCapacity() {
+    return capacity;
+  }
+
+  public Map<Integer, Car> getSlots() {
+    return slots;
+  }
+
+  public int assignSlot(Car car) throws SlotNotAvailableException {
+
+    int nearestSlot = findNearestSlot();
+    slots.put(nearestSlot, car);
+    return nearestSlot;
+  }
+
+  public void freeSlot(int slotNumber) {
+
+    if (slots.containsKey(slotNumber)) {
+      slots.remove(slotNumber);
+    }
+  }
+
+  public int findNearestSlot() {
+
+    for (int slot = 1; slot <= capacity; slot++) {
+      if (!slots.containsKey(slot)) {
+        return slot;
+      }
     }
 
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public Map<Integer, Car> getSlots() {
-        return slots;
-    }
-
-    public int assignSlot(Car car) throws SlotNotAvailableException {
-
-        int nearestSlot = findNearestSlot();
-        slots.put(nearestSlot, car);
-        return nearestSlot;
-    }
-
-    public void freeSlot(int slotNumber){
-
-        if(slots.containsKey(slotNumber)){
-            slots.remove(slotNumber);
-        }
-
-    }
-
-    public int findNearestSlot() throws  SlotNotAvailableException{
-
-        for(int slot = 1; slot <= capacity; slot++){
-               if(!slots.containsKey(slot)){
-                   return slot;
-               }
-        }
-
-        throw new SlotNotAvailableException("No Slots Available");
-    }
+    return -1;
+  }
 }
